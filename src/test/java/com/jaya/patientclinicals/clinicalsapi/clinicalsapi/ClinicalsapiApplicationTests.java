b1d4ad09b1c4a745a7f962718402d367aad0c318
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-01 19:16:59 +0530

    first commit

---
+++ src/test/java/com/jaya/patientclinicals/clinicalsapi/clinicalsapi/ClinicalsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaya.patientclinicals.clinicalsapi.clinicalsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
